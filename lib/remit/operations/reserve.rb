f8e5f410746e51ded2ef56facf743b11b3d8482e
Adam Loving <adamloving@gmail.com>
2012-12-12 16:16:20 -0800

    Only 3 parameters are actually required for 'reserve.'

--- lib/remit/operations/reserve.rb
+++ lib/remit/operations/reserve.rb
@@ -4,17 +4,17 @@ module Remit
   module Reserve
     class Request < Remit::Request
       action :Reserve
-      parameter :recipient_token_id, :required => true
       parameter :sender_token_id, :required => true
+      parameter :caller_reference, :required => true
+      parameter :transaction_amount, :type => Remit::RequestTypes::Amount, :required => true
+      parameter :recipient_token_id
       parameter :sender_reference
       parameter :recipient_reference
-      parameter :caller_reference, :required => true
       parameter :transaction_date
-      parameter :transaction_amount, :type => Remit::RequestTypes::Amount, :required => true
-      parameter :charge_fee_to, :required => true
+      parameter :charge_fee_to
       parameter :sender_description
       parameter :caller_description
-      parameter :marketplace_fixed_fee, :type => Remit::RequestTypes::Amount
+      parameter :marketplace_fixed_fee
       parameter :marketplace_variable_fee
     end
 
