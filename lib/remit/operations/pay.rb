abdab64bdd47d5591b4b1fcf3b1dae2e60e2f59a
Adam Loving <adamloving@gmail.com>
2013-02-07 16:07:54 -0800

    Require fewer parameters for pay

--- lib/remit/operations/pay.rb
+++ lib/remit/operations/pay.rb
@@ -4,16 +4,17 @@ module Remit
   module Pay
     class Request < Remit::Request
       action :Pay
-      parameter :caller_description
+      parameter :sender_token_id, :required => true
       parameter :caller_reference, :required => true
-      parameter :charge_fee_to, :required => true
+      parameter :transaction_amount, :type => Remit::RequestTypes::Amount, :required => true
+
+      parameter :caller_description
+      parameter :charge_fee_to #, :required => true
       parameter :descriptor_policy, :type => Remit::RequestTypes::DescriptorPolicy
       parameter :marketplace_fixed_fee, :type => Remit::RequestTypes::Amount
       parameter :marketplace_variable_fee
       parameter :recipient_token_id #, Only required for MarketPlace transactions
       parameter :sender_description
-      parameter :sender_token_id, :required => true
-      parameter :transaction_amount, :type => Remit::RequestTypes::Amount, :required => true
       parameter :transaction_timeout_in_mins
       parameter :timestamp
     end
